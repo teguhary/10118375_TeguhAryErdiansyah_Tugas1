bf75b95c0f53081208bc7a8fa3476ad383324fb3
aryteguh <41240575+aryteguh@users.noreply.github.com>
2020-03-26 18:17:43 +0700

    Praktikum 1

---
+++ Provis9_Praktikum1/src/KelasUtama.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ary
+ */
+public class KelasUtama extends JFrame{
+    public static void main(String[] args) {
+        JFrameUtama jFrameUtama = new JFrameUtama(); 
+        jFrameUtama.setVisible(true);
+    }
+}
